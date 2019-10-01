e33d5b176286a0a87ebf1f389e0d3fbaf39d8a0c
KeertanaN <keertana_1@yahoo.co.in>
2019-09-30 22:43:43 -0700

    Added route logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,103 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys =require('../../config/keys');
+const passport = require('passport');
 
-router.get('/test', (req, res) => res.json({
-  msg: "User in mnm works!"
-}));
+//@route POST api/users/register
+//@description Registering a user
+//access Public
+
+router.post('/register', (req, res) => {
+  User.findOne({email:req.body.email})
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ email: 'Email already exists' })
+      }
+      else
+      {
+        const avatar = gravatar.url(req.body.email,{s:'200', r:'pg', d:'mm'})
+        const newUser = new User({
+          name:req.body.name,
+          email:req.body.email,
+          password:req.body.password,
+          avatar          
+        });
+        bcrypt.genSalt(10,(err,salt)=> {
+          if (err) throw err;
+          bcrypt.hash(newUser.password,salt,(err,hash) => {
+            //if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+                   .then(user => res.json(user))
+                   .catch(err => console.log(err));
+          });
+        }) 
+      }
+    })
+    .catch(err => console.log(err));
+})
+
+//@route POST api/users/login
+//@description Login a user
+//access Public
+
+router.post('/login',(req,res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email:req.body.email})
+       .then(user => {
+         if(!user){
+           return res.status(404).json({email:"User does not exist in the system."})
+         }
+         
+         bcrypt.compare(password,user.password) 
+               .then(isMatch=> {
+                 if(!isMatch){
+                   return res.status(400).json({password:"Incorrect password!"})
+                 }
+                  const payload={
+                    id:user.id,
+                    name:user.name,
+                    avatar:user.avatar
+                  };
+                 jwt.sign(
+                   payload, 
+                   keys.secretOrKey,
+                   {expiresIn:3600},
+                   (err,token) => {
+                     if (err) throw err;
+                     return res.json({
+                       success:true,
+                       token: 'Bearer '+ token
+                     });
+                   }
+                   );
+                
+               })
+               .catch(err => console.log(err));
+       })
+       .catch(err=>console.log(err));
+})
+
+//@route Get api/users/current - 
+//@description To get the current user
+//access Private
+
+//reading api
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      email: req.user.email,
+      name: req.user.name
+    });
+  });
 
 module.exports = router;
